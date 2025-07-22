324fb0c7cbc047500c5bc1a0f357824d0b1e51a5
asheesh859 <ak2305@checkboxtechnology.com>
2025-07-22 14:41:49 +0530

    add repository and entity class

---
+++ com.spring-security/src/main/java/com/spring_security/entity/Role.java
@@ -0,0 +1,34 @@
+package com.spring_security.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ com.spring-security/src/main/java/com/spring_security/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.spring_security.repository;
+
+import com.spring_security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role , Long> {
+
+}
