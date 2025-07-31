bfe7bab61edf8abd5f207f7d820b4c37061c2d7e
asheesh859 <ak2305@checkboxtechnology.com>
2025-07-31 17:33:46 +0530

    add welcome controller 31/07/2025 5:33 PM

---
+++ security/src/main/java/com/security_psa/security/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.security_psa.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/welcome")
+public class WelcomeController {
+    
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Welcome to the Security Application!";
+    }
+    @GetMapping("/goodbye")
+    public String sayGoodbye() {
+        return "Goodbye, see you next time!";
+    }   
+}
